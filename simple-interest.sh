c4510b3aeffc7f86a534ea18cde25cece9c9d9cb
pranavsingh1910-echo <pranavsingh1910@gmail.com>
2026-09-16 15:13:54 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Simple Interest Calculator"
+
+read -p "Enter Principal Amount: " principal
+read -p "Enter Rate of Interest: " rate
+read -p "Enter Time Period (in years): " time
+
+interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+
+echo "Simple Interest = $interest"
+
+total=$(echo "scale=2; $principal + $interest" | bc)
+
+echo "Total Amount = $total"
